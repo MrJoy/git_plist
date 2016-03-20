e51869e676b751c4e31d039ee1e259c7f4053409
Jon Frisby <jfrisby@mrjoy.com>
2016-03-19 18:53:50 -0700

    Add a couple simple tests...

--- spec/git_plist_spec.rb
+++ spec/git_plist_spec.rb
@@ -5,7 +5,53 @@ describe GitPlist do
     expect(GitPlist::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "format_of" do
+    it "should recognize valid JSON" do
+      sample = <<"_END_"
+        {
+          "int_value": 0,
+          "array_value": ["1","2","3"],
+          "dict_value": {
+            "int_value":0,
+            "string_value": "Hello, world!"
+          },
+          "false_value": false,
+          "true_value": true,
+          "float_value": 0
+        }
+_END_
+      format = GitPlist.format_of(sample.strip)
+      expect(format).to equal(:json)
+    end
+
+    it "should recognize valid XML" do
+      sample = <<"_END_"
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+          <key>string_value</key>
+          <string>Hello, world!</string>
+        </dict>
+        </plist>
+_END_
+      format = GitPlist.format_of(sample.strip)
+      expect(format).to equal(:xml1)
+    end
+  end
+
+  context "simple plist" do
+  end
+
+  context "complex plist" do
+  end
+
+  context "garbage plist" do
+  end
+
+  context "broken plist" do
+  end
+
+  context "empty plist" do
   end
 end
