fc539f0707728aa1e4e25342de94cdcfb4ab7866
Jon Frisby <jfrisby@mrjoy.com>
2016-03-19 19:01:25 -0700

    Change quoting...

--- spec/git_plist_spec.rb
+++ spec/git_plist_spec.rb
@@ -7,7 +7,7 @@ describe GitPlist do
 
   describe "format_of" do
     it "should recognize valid JSON" do
-      sample = <<"_END_"
+      sample = <<_END_
         {
           "int_value": 0,
           "array_value": ["1","2","3"],
@@ -25,7 +25,7 @@ _END_
     end
 
     it "should recognize valid XML" do
-      sample = <<"_END_"
+      sample = <<_END_
         <?xml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
         <plist version="1.0">
